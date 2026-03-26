13a5bb01b34e9d8aa6fec327df232d256dc5d106
nilbalaguer <nilbalaguerfp@ibf.cat>
2026-03-26 19:02:34 +0100

    Update EnemicController.cs

--- Assets/Scripts/Enemic/EnemicController.cs
+++ Assets/Scripts/Enemic/EnemicController.cs
@@ -5,7 +5,7 @@ public class EnemicController : MonoBehaviour
     [SerializeField] Transform guardPoint;
     [SerializeField] float speed = 2f;
 
-    private Transform nextPoint;
+    private Vector3 nextPoint;
 
     void Start()
     {
@@ -19,9 +19,9 @@ public class EnemicController : MonoBehaviour
 
     void MoveToPoint()
     {
-        transform.position = Vector3.MoveTowards(transform.position, nextPoint.position, speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, nextPoint, speed * Time.deltaTime);
 
-        if (Vector3.Distance(transform.position, nextPoint.position) < 0.1f)
+        if (Vector3.Distance(transform.position, nextPoint) < 0.1f)
         {
             GenerarPoint();
         }
@@ -30,6 +30,6 @@ public class EnemicController : MonoBehaviour
     void GenerarPoint()
     {
         Vector3 tempTrans = new Vector3(guardPoint.position.x + Random.Range(-5f, 5f), 0f, guardPoint.position.z + Random.Range(-5f, 5f));
-        nextPoint.position = tempTrans;
+        nextPoint = tempTrans;
     }
 }
