a4ae00954ad0e471155a9659e054812707266db8
nilbalaguer <nilbalaguerfp@ibf.cat>
2026-04-07 20:00:32 +0200

    Arbre de comportament minimament funcional

--- Assets/Scripts/Enemic/EnemicController.cs
+++ Assets/Scripts/Enemic/EnemicController.cs
@@ -8,15 +8,17 @@ public class EnemicController : MonoBehaviour
     [SerializeField] float guardDistance = 6f;
 
     public float energia = 100f;
-    private GameObject perseguint;
+    public GameObject perseguint;
+    public GameObject minionDintreRadi;
 
-    private Vector3 nextPoint;
+    public Vector3 nextPoint;
 
-    private NavMeshAgent agent;
-    
+    public NavMeshAgent agent;
+
 
     void Start()
     {
+        Invoke("GenerarPoint", 10f);
         agent = GetComponent<NavMeshAgent>();
         GenerarPoint();
     }
@@ -26,29 +28,52 @@ public class EnemicController : MonoBehaviour
         //Arbre de comportament
         if ( Vector3.Distance(transform.position, guardPoint.position) > guardDistance) //Esta lluny de la font?
         {
-            
+            MoveToCenter();
+            Debug.Log("Lluny de la font");
         }
         else
         {
             if (energia < 30f) //La energia es mes petita que 30%?
             {
                 MoveToCenter();
+                Debug.Log("Energia baixa");
+                if (Vector3.Distance(transform.position, guardPoint.position) < 2f)
+                {
+                    Debug.Log("Curant");
+                    energia += Time.deltaTime * 2f;
+                }
             }
             else
             {
                 if (perseguint != null) //Perseguint?
                 {
-                    
+                    Debug.Log("Perseguint");
+                    MoveToMinion();
                 }
                 else
                 {
-                    MoveToPoint();
+                    if (minionDintreRadi != null) //Hi ha un minion dintre del meu radi? 
+                    {
+                        perseguint = minionDintreRadi;
+                        minionDintreRadi = null;
+                    }
+                    else
+                    {
+                        //Wonder
+                        Debug.Log("Viatjant al punt");
+                        MoveToPoint();
+                    }
                 }
             }
         }
 
 
         
+    }
+
+    void MoveToMinion()
+    {
+        agent.SetDestination(perseguint.GetComponent<Transform>().position);
     }
 
     void MoveToPoint()
@@ -65,11 +90,29 @@ public class EnemicController : MonoBehaviour
     void MoveToCenter()
     {
         agent.SetDestination(guardPoint.position);
+        perseguint = null;
     }
 
     void GenerarPoint()
     {
-        Vector3 tempTrans = new Vector3(guardPoint.position.x + Random.Range(-guardDistance, guardDistance), 0f, guardPoint.position.z + Random.Range(-guardDistance, guardDistance));
-        nextPoint = tempTrans;
+        Vector2 randomCircle = Random.insideUnitCircle.normalized * guardDistance;
+
+        Vector3 randomPoint = new Vector3(
+            guardPoint.position.x + randomCircle.x,
+            guardPoint.position.y,
+            guardPoint.position.z + randomCircle.y
+        );
+
+        if (NavMesh.SamplePosition(randomPoint, out NavMeshHit hit, guardDistance, NavMesh.AllAreas))
+        {
+            nextPoint = hit.position;
+        }
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("minion"))
+        {
+            minionDintreRadi = other.gameObject;
+        }
     }
 }
