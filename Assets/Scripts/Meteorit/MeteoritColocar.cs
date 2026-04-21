2111f8e176beaf625566911f4962c751b59583fa
nilbalaguer <nilbalaguerfp@ibf.cat>
2026-04-21 20:05:20 +0200

    colocador de peçes en la nau

--- Assets/Scripts/Meteorit/MeteoritColocar.cs
+++ Assets/Scripts/Meteorit/MeteoritColocar.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MeteoritColocar : MonoBehaviour
 {
     [SerializeField] Transform[] posicions;
+    [SerializeField] Transform[] objectes;
     [SerializeField] bool[] posicionats;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -14,11 +16,38 @@ public class MeteoritColocar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void ColocarObjecte(int identificador)
+    private void Actualitzar()
     {
-        posicionats[identificador] = true;
+        for (int i = 0; i < objectes.Length; i++)
+        {
+            if (posicionats[i])
+            {
+                objectes[i].position = posicions[i].position;
+                objectes[i].rotation = posicions[i].rotation;
+
+                Rigidbody tempRigid = objectes[i].gameObject.GetComponent<Rigidbody>();
+
+                tempRigid.isKinematic = true;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("meteorPart"))
+        {
+            for (int i = 0; i < objectes.Length; i++)
+            {
+                if (objectes[i] == other.transform)
+                {
+                    posicionats[i] = true;
+                    break;
+                }
+            }
+
+            Actualitzar();
+        }
     }
 }
