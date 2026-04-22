fca6913e8ac063548c478fe9bc545b2d9be14881
nilbalaguer <nilbalaguerfp@ibf.cat>
2026-04-22 20:45:24 +0200

    poblado y movimiento suave de las partes al meteorito

--- Assets/Scripts/Meteorit/MeteoritColocar.cs
+++ Assets/Scripts/Meteorit/MeteoritColocar.cs
@@ -1,37 +1,59 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
+/*
+Aquest script coloca les peçes de la nau / meteorit al lloc on toquen de manera suau.
+*/
 public class MeteoritColocar : MonoBehaviour
 {
-    [SerializeField] Transform[] posicions;
-    [SerializeField] Transform[] objectes;
-    [SerializeField] bool[] posicionats;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Tooltip("Pocicions objectiu")]
+    [SerializeField] Transform[] posicions; //Pocicions objectiu
+    [Tooltip("Objectes que s'han de posicionar")]
+    [SerializeField] Transform[] objectes; //Objectes que s'han de posicionar
+    [Tooltip("Objectes que han sigut posicionats")]
+    [SerializeField] bool[] posicionats; //Objectes que han sigut posicionats
+    private bool enMoviment = false;
 
     // Update is called once per frame
     void Update()
     {
-
+        if (enMoviment)
+        {
+            Actualitzar();
+        }
     }
 
     private void Actualitzar()
     {
+        int trues = 0;
+        int totals = 0;
+
         for (int i = 0; i < objectes.Length; i++)
         {
             if (posicionats[i])
             {
-                objectes[i].position = posicions[i].position;
+                totals += 1;
+
+                if (objectes[i].position != posicions[i].position)
+                {
+                    objectes[i].position = Vector3.MoveTowards(objectes[i].position, posicions[i].position, 1f * Time.deltaTime);
+                } else
+                {
+                    trues += 1;
+                }
+
                 objectes[i].rotation = posicions[i].rotation;
 
                 Rigidbody tempRigid = objectes[i].gameObject.GetComponent<Rigidbody>();
 
                 tempRigid.isKinematic = true;
             }
+        }
+
+        if (totals == trues)
+        {
+            enMoviment = false;
         }
     }
 
@@ -47,7 +69,7 @@ public class MeteoritColocar : MonoBehaviour
                 }
             }
 
-            Actualitzar();
+            enMoviment = true;
         }
     }
 }
