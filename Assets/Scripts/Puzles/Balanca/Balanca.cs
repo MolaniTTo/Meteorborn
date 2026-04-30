1a29d49f6c912a94f2e188addb760e8cc8276f11
nilbalaguer <nilbalaguerfp@ibf.cat>
2026-04-30 20:17:05 +0200

    Balança semifuncional

---
+++ Assets/Scripts/Puzles/Balanca/Balanca.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Balanca : MonoBehaviour
+{
+    [SerializeField] PlataformaBalanca plataformaBalanca1;
+    [SerializeField] PlataformaBalanca plataformaBalanca2;
+
+    [SerializeField] Transform rotadorBalanca;
+
+    private float rotacioObjectiu = 0f;
+    private bool actualitzant = false;
+
+    void Update()
+    {
+        if (actualitzant)
+        {
+            Quaternion rotacioFinal = Quaternion.Euler(0f, 0f, rotacioObjectiu);
+
+            rotadorBalanca.rotation = Quaternion.RotateTowards(
+                rotadorBalanca.rotation,
+                rotacioFinal,
+                3f * Time.deltaTime
+            );
+
+            if (Quaternion.Angle(rotadorBalanca.rotation, rotacioFinal) < 0.1f)
+            {
+                actualitzant = false;
+            }
+        }
+    }
+
+    public void Actualitzar()
+    {
+        float pesResult = plataformaBalanca1.pess - plataformaBalanca2.pess;
+
+        rotacioObjectiu = pesResult * 3f;
+
+        actualitzant = true;
+    }
+}
--- Assets/Scripts/Puzles/Balanca/PlataformaBalanca.cs
+++ Assets/Scripts/Puzles/Balanca/PlataformaBalanca.cs
@@ -4,11 +4,16 @@ public class PlataformaBalanca : MonoBehaviour
 {
     public float pess;
     [SerializeField] Transform plataformaTransform;
+    [SerializeField] Balanca balanca;
 
     public Vector3 objectivePosition;
+    public Vector3 initialPosition;
+
+    private float miniCounter;
 
     private void Start() {
         objectivePosition = plataformaTransform.position;
+        initialPosition = objectivePosition;
     }
 
     private void FixedUpdate() {
@@ -16,6 +21,21 @@ public class PlataformaBalanca : MonoBehaviour
         {
             plataformaTransform.position = Vector3.MoveTowards(plataformaTransform.position, objectivePosition, 0.01f);
         }
+
+        if (miniCounter >= 2f)
+        {
+            if (pess == 0)
+            {
+                objectivePosition = initialPosition;
+            } else
+            {
+                objectivePosition[1] = initialPosition[1] - pess * 0.2f;
+            }
+
+            miniCounter = 0f;
+        }
+
+        miniCounter += Time.deltaTime;
     }
 
     private void OnTriggerEnter(Collider other) {
@@ -24,7 +44,9 @@ public class PlataformaBalanca : MonoBehaviour
             Rigidbody temporalRb = other.GetComponent<Rigidbody>();
             pess += temporalRb.mass;
 
-            objectivePosition = new Vector3(plataformaTransform.position.x, plataformaTransform.position.y + -pess / 4, plataformaTransform.position.z);
+            objectivePosition = new Vector3(plataformaTransform.position.x, initialPosition[1] - temporalRb.mass * 0.2f, plataformaTransform.position.z);
+
+            balanca.Actualitzar();
         }
     }
 
@@ -33,10 +55,11 @@ public class PlataformaBalanca : MonoBehaviour
         {
             Rigidbody temporalRb = other.GetComponent<Rigidbody>();
 
-            objectivePosition = new Vector3(plataformaTransform.position.x, plataformaTransform.position.y + pess / 4, plataformaTransform.position.z);
+            objectivePosition = new Vector3(plataformaTransform.position.x, initialPosition[1] + temporalRb.mass * 0.2f, plataformaTransform.position.z);
 
             pess -= temporalRb.mass;
 
+            balanca.Actualitzar();
         }
     }
 }
